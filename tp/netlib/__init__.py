098970fdb647c498248a9433bc243d115e09f202
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-05-14 10:59:18 +0200

    tp.netlib exports version and install path now.

--- tp/netlib/__init__.py
+++ tp/netlib/__init__.py
@@ -1,10 +1,9 @@
-"""
 import sys
 from os import path
+
 sys.path.insert(0, path.dirname(__file__))
 
-import objects
-from objects import constants
+from version import version as __version__
+from version import installpath as __installpath__
 
 sys.path.pop(0)
-"""
